61a0f0d88282b2881c0aa830c50eccfb4dfdbff0
sarvagya230 <68740017+sarvagya230@users.noreply.github.com>
2023-03-18 00:08:51 +0530

    footer and model

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+let country;
+document.querySelector('.model').style.opacity=0
+console.log("yo")
+fetch('https://api.ipregistry.co/2401:4900:3b36:b3d2:85fd:ced6:e2:660?key=nnym2r91gi242170')
+    .then(function (response) {
+       let ipdata=response.json().then((data) =>{
+        country= data.location.country.name
+        document.querySelector('.countryText').innerHTML=`cyber crime rate in ${country}`
+        document.querySelector('.main').style.opacity="1";loader.style.opacity="0";document.querySelector('body').style.overflowY="visible";chart.deltaLatitude=-data.location.latitude;chart.deltaLongitude=-data.location.longitude;setTimeout(()=>{
+            let opacity=0;
+            let interval=setInterval(()=>{
+                document.querySelector('.model').style.opacity=opacity;opacity+=0.1;if(opacity==1){clearInterval(interval)}},1)
+               },100)})
+       
+       fetch('./country-codes.json',(data)=>{
+        
+       })
+    }).catch(error => {console.log(error)})
 const questions=[{
     "question":"Which of the following is NOT a common method of social engineering? ",
     "answer":2,
@@ -116,51 +134,7 @@ polygonSeries.useGeodata = true;
 var polygonTemplate = polygonSeries.mapPolygons.template;
 polygonTemplate.tooltipText = "{name}";
 polygonTemplate.stroke = am4core.color("#000");
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition((pos)=>{
 
-       
-        chart.deltaLongitude=-pos.coords.longitude;
-        chart.deltaLatitude = -pos.coords.latitude;
-        console.log(pos.coords.longitude)
-        
-       polygonSeries.data.forEach((e)=>{
-            if(e.id==="IN"){
-                console.log(e)
-            }
-        })
-        document.querySelector('.main').style.opacity="1";loader.style.opacity="0";document.querySelector('body').style.overflowY="visible"
-
-    },()=>
-{    console.log("yo")
-fetch('https://api.ipregistry.co/2401:4900:3b36:b3d2:85fd:ced6:e2:660?key=nnym2r91gi242170')
-    .then(function (response) {
-       let ipdata=response.json().then((data) =>{
-        console.log("Success")
-        document.querySelector('.main').style.opacity="1";loader.style.opacity="0";document.querySelector('body').style.overflowY="visible";chart.deltaLatitude=-data.location.latitude;chart.deltaLongitude=-data.location.longitude})
-       
-       fetch('./country-codes.json',(data)=>{
-        
-       })
-    }).catch(error => {console.log(error)})
-   
-   ;})
-        
-
-   
-
-
-}
-else{
-    fetch('https://api.ipregistry.co/2401:4900:3b36:b3d2:85fd:ced6:e2:660?key=nnym2r91gi242170')
-    .then(function (response) {
-       let ipdata=response.json()
-       fetch('./country-codes.json',(data)=>{
-        console.log(ipdata,data)
-       })
-    })
-    document.querySelector('.main').style.opacity="1";loader.style.opacity="0";document.querySelector('body').style.overflowY="visible"
-}
 
 chart.geodata = am4geodata_worldLow;
 chart.projection = new am4maps.projections.Orthographic();
@@ -185,3 +159,17 @@ chart.seriesContainer.events.on("down", function(){
 chart.seriesContainer.events.on("up", function(){
     animation.resume()
 })
+//model controller
+window.addEventListener('click', function(e){   
+    if (document.querySelector('.data-div').contains(e.target)){
+        
+      
+    } else{
+        modelClose()
+    }
+  });
+document.querySelector('.cross').addEventListener("click",()=>{modelClose()})  
+function modelClose(){
+    document.querySelector('.model').remove()
+
+  }
